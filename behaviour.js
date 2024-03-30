a65cc1dc4dd5efa03746d8c08310c6e1b708b2e1
Marc Frances <marc.frances.t@gmail.com>
2024-03-30 14:33:32 +0100

    Add new behaviour for numbers grid
    Now you can click on a number element to guess if that is the next
    digit of the introduced number

--- behaviour.js
+++ behaviour.js
@@ -1,24 +1,31 @@
+import {
+  setGridElementActive,
+  isGridElementCorrect,
+} from "./modules/grid_utilities.mjs";
+
 let fullNumber;
 let currentNumber;
 
-const form = document.getElementsByTagName('form')[0];
-const resetBtn = document.getElementById('reset-btn');
-const grid = document.querySelector('.number-grid');
-const nextBtn = document.getElementById('next-btn');
+const form = document.getElementsByTagName("form")[0];
+const resetBtn = document.getElementById("reset-btn");
+const nextBtn = document.getElementById("next-btn");
+const hideBtn = document.getElementById("hide-btn");
+const gridElements = document.querySelectorAll(".number-element");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const numberInput = document.querySelector('form input[type="number"]');
-  fullNumber = numberInput.value.replace(/\D/g, ''); // Solo digitos
+  fullNumber = numberInput.value.replace(/\D/g, ""); // Solo digitos
   currentNumber = fullNumber;
 
   if (currentNumber.length <= 0) {
-    alert('Introduce algún número');
+    alert("Introduce algún número");
     return;
   }
+  numberInput.value = "";
 
-  const currentNumberP = document.querySelector('p.current-number');
+  const currentNumberP = document.querySelector("p.current-number");
   currentNumberP.textContent = fullNumber;
 
   //Inicializar grid
@@ -26,48 +33,48 @@ form.addEventListener('submit', (e) => {
   currentNumber = currentNumber.slice(1);
 });
 
-resetBtn.addEventListener('click', (e) => {
+resetBtn.addEventListener("click", (e) => {
   currentNumber = fullNumber;
 
   //Inicializar grid
-  if (currentNumber.length > 0) {
-    setGridElementActive(currentNumber.charAt(0));
-    currentNumber = currentNumber.slice(1);
-  } else {
-    alert('Introduce algún número');
-  }
+  if (!previousChecks()) return;
+  setGridElementActive(currentNumber.charAt(0));
+  currentNumber = currentNumber.slice(1);
 });
 
-nextBtn.addEventListener('click', (e) => {
-  if (currentNumber.length > 0) {
-    setGridElementActive(currentNumber.charAt(0));
-    currentNumber = currentNumber.slice(1);
-  } else {
-    alert('Último número de la secuencia');
-  }
+nextBtn.addEventListener("click", (e) => {
+  if (!previousChecks()) return;
+  setGridElementActive(currentNumber.charAt(0));
+  currentNumber = currentNumber.slice(1);
 });
 
-const gridElements = {
-  0: document.document.getElementById('zero'),
-  1: document.document.getElementById('one'),
-  2: document.document.getElementById('two'),
-  3: document.document.getElementById('three'),
-  4: document.document.getElementById('four'),
-  5: document.document.getElementById('five'),
-  6: document.document.getElementById('six'),
-  7: document.document.getElementById('seven'),
-  8: document.document.getElementById('eight'),
-  9: document.document.getElementById('nine'),
-};
+hideBtn.addEventListener("click", (e) => {
+  const currentNumberP = document.querySelector("p.current-number");
+  currentNumberP.classList.toggle("hide");
 
-function setGridElementActive(digit) {
-  const element = gridElements[digit];
-  if (!element) {
-    alert('Avisar a Marc');
-    throw new Error('Digit does not correspond with any grid element');
+  const name = e.target.textContent;
+  e.target.textContent = name == "Hide" ? "Show" : "Hide";
+});
+
+gridElements.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (!previousChecks()) return;
+
+    if (isGridElementCorrect(e.target, currentNumber.charAt(0))) {
+      setGridElementActive(currentNumber.charAt(0));
+      currentNumber = currentNumber.slice(1);
+    }
+  });
+});
+
+function previousChecks() {
+  if (currentNumber === undefined) {
+    alert("Introduzca un numero");
+    return false;
+  } else if (currentNumber.length <= 0) {
+    alert("Último número de la secuencia");
+    return false;
   }
-  for (let i = 0; i < grid.children.length; i++) {
-    grid.children[i].classList.remove('active');
-  }
-  element.classList.add('active');
+
+  return true;
 }
